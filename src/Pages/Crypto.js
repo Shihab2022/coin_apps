37ba5ba689434a28a8336658b914dc2351576831
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-10-21 14:41:16 +0600

    clickable all currencey

--- src/Pages/Crypto.js
+++ src/Pages/Crypto.js
@@ -12,15 +12,16 @@ const Crypto = () => {
     fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
       .then((response) => response.json())
       .then((data) => setValue(data));
-  }, []);
-  console.log(value);
+  }, [coinId,values]);
+  console.log(values);
 
   return (
     <>
       <div className="grid grid-cols-12 ">
         {/* crypto List */}
-        <div className="col-start-1 col-end-3 py-10 px-5 font-mono  bg-red-500 ">
-          <h1 className="text-2xl font-bold uppercase py-2 font-serif text-amber-500">
+        <div className="col-start-1 col-end-3 py-10 px-2 font-mono  bg-red-100 ">
+        <div className="mb-3">
+        <h1 className="text-2xl font-bold uppercase py-2 font-serif text-amber-500">
             Crypto List
           </h1>
           <form>
@@ -32,18 +33,20 @@ const Crypto = () => {
             />
             <input type="submit" value="" />
           </form>
+        </div>
           {values?.slice(0, 20)?.map((d) => (
             <>
-              <div className="flex text-lg my-5 justify-between overflow-hidden">
+              <div onClick={()=>setCoinId(d?.id)} className="flex duration-500 hover:bg-red-200 cursor-pointer text-lg  p-3 justify-between overflow-hidden">
                 <p>{d?.rank}</p>
-                <p className="mx-3">{d?.name}</p>
+                <p className="mx-3 overflow-hidden">{d?.name}</p>
                 <p className=" text-pink-800">{d?.symbol}</p>
               </div>
             </>
           ))}
+         
         </div>
         {/* details list */}
-        <div className="col-start-3 col-end-13 px-10 py-14 space-x-2 bg-amber-500 ">
+        <div className="col-start-3 col-end-13 px-10 py-14 space-x-2 bg-amber-100 ">
           <div>
             <div className="flex justify-start text-xl font-bold">
               <p>{value?.name}</p>
@@ -52,16 +55,16 @@ const Crypto = () => {
             <p>{value?.description}</p>
             <p> Hash Algorithm : {value?.hash_algorithm}</p>
             <p> Structure Organisation : {value?.org_structure}</p>
-            <div>
-              <p>People who worked on {value?.name}</p>
+            <div className="">
+              <p className="text-xl my-3 font-bold">People who worked on <span className="text-pink-800">{value?.name} </span>: </p>
               {value?.team?.map((d) => (
-                <p>
+                <p className="text-lg">
                   {d?.name} <span className="text-sm ml-3">({d?.position})</span>
                 </p>
               ))}
             </div>
-            {/* <a className="block" href={value?.links_extended[0]?.url}>Official Website</a> */}
-            <div>
+            {/* <a className="block" href={value?.links_extended[0]?.url} target="_blank">Official Website</a> */}
+            <div className="text-xl font-bold mt-3">
                 <span>Read More About : </span>
             <button className="text-xl font-semibold text-pink-900">{value?.name}</button>
             </div>
